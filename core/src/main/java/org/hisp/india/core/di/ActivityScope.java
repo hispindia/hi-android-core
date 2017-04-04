c3988bcbfcdf1f6789bdc61338cd34d0acaa7058
Nhan Cao <nhancv92@gmail.com>
2017-04-05 00:06:37 +0700

    update gradle

---
+++ core/src/main/java/org/hisp/india/core/di/ActivityScope.java
@@ -0,0 +1,15 @@
+package org.hisp.india.core.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by nhancao on 4/5/17.
+ */
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
---
+++ core/src/main/java/org/hisp/india/core/services/NetworkProvider.java
@@ -0,0 +1,11 @@
+package org.hisp.india.core.services;
+
+/**
+ * Created by nhancao on 4/5/17.
+ */
+
+public interface NetworkProvider {
+
+    boolean isNetworkAvailable();
+
+}
